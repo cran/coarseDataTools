61521e59eb29ba73c58823178ea4f8322b2f6d72
Nicholas G. Reich <nick@schoolph.umass.edu>
2013-08-05 10:02:42 +0000

    version 0.6-2

--- R/sampleSizeSimulation.R
+++ R/sampleSizeSimulation.R
@@ -14,6 +14,50 @@
 ## pct.type.A = percent type A data, will be rounded up to nearest integer
 ## exp.win.dat = a vector of exposure window lengths to sample from
 ## verb = whether to print 10 iteration counts during the course of the sim
+
+
+##' @name precision.simulation
+##' @aliases precision.simulation.exact
+##' @aliases precision.simulation.coarse
+##' @aliases generate.coarse.data
+##'   
+##' @title Simulate incubation period analyses with coarse data
+##'   
+##' @description These functions simulate coarse incubation period data sets and
+##'   analyze them.  The goal is for these simulations to provide evidence for
+##'   how much information a given dataset contains about a characteristic of
+##'   the incubation period distribution.
+##'   
+##' @param N Overall sample size for the datasets to be simulated.
+##' @param med Median for the assumed log normal distribution of the incubation
+##'   periods.
+##' @param disp Dispersion for the assumed log normal distribution of the
+##'   incubation periods.
+##' @param percentile Percentile of the incubation period distribution which we
+##'   want to estimate.
+##' @param nsim Number of datasets to analyze in the simulation.
+##' @param exact.data Either TRUE/FALSE.  Incidates whether the data generated
+##'   should be coarsened at all.  If TRUE, pct.type.A and exp.win.dat are
+##'   ignored.
+##' @param pct.type.A Percent of the N observations that are assumed to be type
+##'   A data.  If N*pct.type.A is not an integer, it will be rounded to the
+##'   nearest integer.
+##' @param exp.win.dat A vector of exposure window lengths.  Defaults to the
+##'   observed window lengths from Lessler et al. (see below).
+##' @param verb If TRUE, a message with the system time and iteration number
+##'   will be printed ten times during the simulation run.
+##'   
+##'   
+##' @rdname precision.simulation
+##' @return The \code{precision.simulation} functions return a matrix with four
+##'   columns and nsim rows.  The "ests" column gives the estimated percentiles
+##'   for the incubation period distribution.  The "SE" column gives the
+##'   standard error for the estimate.  The "conv" column is 1 if the doubly
+##'   interval-censored likelihood maximization converged.  Otherwise, it is 0.
+##'   The "bias" column gives the estimated percentile - true percentile. The
+##'   \code{generate.coarse.data} function returns a matrix with data suitable
+##'   for analysis by the \code{dic.fit} function.
+##' @export
 precision.simulation <- function(N,
 			       med=2,
 			       disp=1.3,
@@ -30,8 +74,7 @@ precision.simulation <- function(N,
 		stop("% of data that is type A must be between 0 and 1.")
 	if(is.null(exp.win.dat)){
 		if(verb) message("NYC exposure window data used")
-		#data("exp.win.lengths")
-		exp.win.dat <- get(data("exp.win.lengths"))
+		exp.win.dat <- get(data(exp.win.lengths, envir = environment()))
 	}
 
 	## TODO: add default exp.win.dat to package and load if NULL
@@ -61,44 +104,46 @@ precision.simulation <- function(N,
 	return(out)
 }
 
-
+##' @rdname precision.simulation
+##' @export
 precision.simulation.exact <- function(N,
-				     med,
-				     disp,
-				     percentile,
-				     nsim,
-				     verb) {
-	storage <- matrix(NA, ncol=3, nrow=nsim)
-	colnames(storage) <- c("ests", "SE", "conv")
+                                       med,
+                                       disp,
+                                       percentile,
+                                       nsim,
+                                       verb) {
+    storage <- matrix(NA, ncol=3, nrow=nsim)
+    colnames(storage) <- c("ests", "SE", "conv")
 
-	data <- matrix(0, ncol=6, nrow=nsim*N)
-	colnames(data) <- c("dataset.id", "EL", "ER", "SL", "SR", "type")
-	data[,"dataset.id"] <- rep(1:nsim, each=N)
-	data[,"SL"] <- rlnorm(N*nsim, meanlog=log(med), sdlog=log(disp))
-	data[,"SR"] <- data[,"SL"]
-	data[,"type"] <- 2
+    data <- matrix(0, ncol=6, nrow=nsim*N)
+    colnames(data) <- c("dataset.id", "EL", "ER", "SL", "SR", "type")
+    data[,"dataset.id"] <- rep(1:nsim, each=N)
+    data[,"SL"] <- rlnorm(N*nsim, meanlog=log(med), sdlog=log(disp))
+    data[,"SR"] <- data[,"SL"]
+    data[,"type"] <- 2
 
-	for(i in 1:nsim){
-		tmp.dat <- data[which(data[,"dataset.id"]==i),]
-		tmp.fit <- dic.fit(tmp.dat, ptiles=percentile)
-		if(tmp.fit$conv==1){
-			row.name <- paste("p", round(percentile*100), sep="")
-			which.row <-
-				which(rownames(tmp.fit$ests)==row.name)[1]
-			storage[i,c("ests", "SE")] <-
-				tmp.fit$ests[which.row, c("est", "StdErr")]
-		} else {
-			storage[i,c("ests", "SE")] <- NA
-		}
-		storage[i,"conv"] <- tmp.fit$conv
-		if(verb & i%%(round(nsim/10))==0)
+    for(i in 1:nsim){
+        tmp.dat <- data[which(data[,"dataset.id"]==i),]
+        tmp.fit <- dic.fit(tmp.dat, ptiles=percentile)
+        if(tmp.fit$conv==1){
+            row.name <- paste("p", round(percentile*100), sep="")
+            which.row <-
+                which(rownames(tmp.fit$ests)==row.name)[1]
+            storage[i,c("ests", "SE")] <-
+                tmp.fit$ests[which.row, c("est", "StdErr")]
+        } else {
+            storage[i,c("ests", "SE")] <- NA
+        }
+        storage[i,"conv"] <- tmp.fit$conv
+        if(verb & i%%(round(nsim/10))==0)
 			print(paste("iteration",i,"complete ::", Sys.time()))
 
-	}
-	return(storage)
+    }
+    return(storage)
 }
 
-
+##' @rdname precision.simulation
+##' @export
 precision.simulation.coarse <- function(N,
 				      med,
 				      disp,
@@ -121,7 +166,7 @@ precision.simulation.coarse <- function(N,
 		if(tmp.fit$conv==1){
 			row.name <- paste("p", round(percentile*100), sep="")
 			which.row <-
-				which(rownames(tmp.fit$ests)==row.name)[1]
+                            which(rownames(tmp.fit$ests)==row.name)[1]
 			storage[i,c("ests", "SE")] <-
 				tmp.fit$ests[which.row, c("est", "StdErr")]
 		} else {
@@ -135,10 +180,11 @@ precision.simulation.coarse <- function(N,
 }
 
 
-
+##' @rdname precision.simulation
+##' @export
 generate.coarse.data <- function(N, med, disp, pct.type.A, exp.win.dat) {
 
-	n.type.A <- round(N*pct.type.A)
+ 	n.type.A <- round(N*pct.type.A)
 	n.type.B <- N-n.type.A
 
 
